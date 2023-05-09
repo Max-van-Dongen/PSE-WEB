ac474793bc8f9574d97201aca42de9ab6a9a7889
Exellar <radiomixonly@gmail.com>
2023-05-09 17:32:58 +0200

    Update server.js

--- ws/server.js
+++ ws/server.js
@@ -18,8 +18,15 @@ const wss = new ws.Server({ server });
 var clients = [];
 wss.on('connection', function connection(ws,request,client) {
   ws.on('error', console.error);
-  clients.push(client);
-  console.log(clients);
+  const userId = uuid.v4();
+  map.set(userId, ws);
+//   clients.push(client);
+//   console.log(clients);
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+        console.log(userId);
+    }
+  });
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
