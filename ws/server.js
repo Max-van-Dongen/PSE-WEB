f9235bbf0d4951279b1d613f8538dfbbc912cfd9
Exellar <radiomixonly@gmail.com>
2023-05-09 10:39:57 +0200

    WSS Testing

--- ws/server.js
+++ ws/server.js
@@ -1,15 +1,19 @@
 const ws =  require('ws');
 const fs =  require('fs');
-// const server = createServer({
-//     // cert: ws.readFileSync('certs/cert.pem'),
-//     // key: ws.readFileSync('certs/key.pem'),
-//     port: 8089
-//   });
-const wss = new ws.WebSocketServer({
-    port: 8089,
+const https =  require('https');
+const server = new https.createServer({
     cert: fs.readFileSync('certs/server.crt'),
     key: fs.readFileSync('certs/server.key'),
-});
+    port: 8089,
+  });
+//   console.log('');
+// const wss = new ws.WebSocketServer({
+//     port: 8089,
+//     cert: fs.readFileSync('certs/server.crt'),
+//     key: fs.readFileSync('certs/server.key'),
+// });
+const wss = new ws.Server({ server });
+// console.log(wss);
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
