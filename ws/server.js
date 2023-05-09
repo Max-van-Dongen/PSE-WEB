458e8af12860f6804091d33d0fb297f6f5da223a
Exellar <radiomixonly@gmail.com>
2023-05-09 17:46:19 +0200

    ad uuids

--- ws/server.js
+++ ws/server.js
@@ -1,57 +1,49 @@
-const ws =  require('ws');
-const fs =  require('fs');
-const https =  require('https');
+const ws = require('ws');
+const fs = require('fs');
+const https = require('https');
+const uuid = require('uuid');
+
+const clients = new Map(); // Map to store the WebSocket connections with their UUIDs
+
 const server = new https.createServer({
     cert: fs.readFileSync('certs/server.crt'),
     key: fs.readFileSync('certs/server.key'),
     rejectUnauthorized: false,
-    // port: 8089,
-  });
-//   console.log('');
-// const wss = new ws.WebSocketServer({
-//     port: 8089,
-//     cert: fs.readFileSync('certs/server.crt'),
-//     key: fs.readFileSync('certs/server.key'),
-// });
-const wss = new ws.Server({ server });
-// console.log(wss);
-var clients = [];
-wss.on('connection', function connection(ws,request,client) {
-  ws.on('error', console.error);
-  const userId = uuid.v4();
-  map.set(userId, ws);
-//   clients.push(client);
-//   console.log(clients);
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-        console.log(userId);
-    }
-  });
-
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-    if (data == "hoi") {
-        ws.send("hello");
-    }
-    if (data == "doei") {
-        ws.send("goodbye");
-    }
-    // ws.send('something');
-  });
-  ws.on('upgrade', (request, socket, head) => {
-    const origin = request && request.headers && request.headers.origin;
-    const corsRegex = /^https?:\/\/(.*\.?)abc\.com(:\d+)?\/$/g
-    if (origin && origin.match(corsRegex) != null) {
-      wss.handleUpgrade(request, socket, head, (ws) => {
-        wss.emit('connection', ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-  });
-
-
-  ws.send('Welcome!');
 });
+
+const wss = new ws.Server({ server });
+wss.on('connection', function connection(ws, request, client) {
+    const clientId = uuid.v4(); // Generate a unique UUID for the client
+    clients.set(clientId, ws); // Add the WebSocket connection to the clients map
+    console.log(`Client connected: ${clientId}`);
+
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+        if (data == 'hoi') {
+            ws.send('hello');
+        }
+        if (data == 'doei') {
+            ws.send('goodbye');
+        }
+        if (data == 'hey') {
+            clients.forEach((clientWs, clientUUID) => {
+                if (clientUUID !== clientId) { // Don't send the message back to the sender
+                    clientWs.send(`Client ${clientId} says: hey`);
+                }
+            });
+        }
+    });
+
+    ws.on('close', function close() {
+        // Remove the WebSocket connection of the disconnected client
+        clients.delete(clientId);
+        console.log(`Client disconnected: ${clientId}`);
+    });
+
+    ws.send('Welcome!');
+});
+
 server.listen(8089);
 server.setMaxListeners(0);
